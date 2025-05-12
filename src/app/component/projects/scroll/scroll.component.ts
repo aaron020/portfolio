83c923e6d9145c72818459a7729a9fab58c6e011
aaronmeade <aaronmeade1@gmail.com>
2025-05-12 21:32:07 +0100

    updating data entry

--- src/app/component/projects/scroll/scroll.component.ts
+++ src/app/component/projects/scroll/scroll.component.ts
@@ -13,17 +13,24 @@ export class ScrollComponent {
   cards: CardData[] = [
     {
       title: 'Topic Test',
-      description: '          Interactive learning platform enabling users to create personalized topic-based assessments, manage question banks, and participate in both self-created and community-generated tests.',
+      description: 'Interactive learning platform enabling users to create personalized topic-based assessments, manage question banks, and participate in both self-created and community-generated tests.',
       imageUrl: 'https://portfolio-info-site-csdcd.s3.eu-west-1.amazonaws.com/about-me/project-pic/topic-test.png',
       tag: 'AWS',
       buttonLink: '/topic-test'
     },
     {
-      title: 'Data Entry System',
-      description: 'This is a description for card 2. Add your content here.',
-      imageUrl: 'https://example.com/image2.jpg',
+      title: 'Data Entry (Real World Application)',
+      description: 'This project was something I worked on for a small local business, a modern data entry system and document generator.',
+      imageUrl: 'https://portfolio-info-site-csdcd.s3.eu-west-1.amazonaws.com/project/data-entry/home_edited.png',
       tag: 'FrontEnd',
       buttonLink: '/data-entry'
+    },
+    {
+      title: 'MeGPT',
+      description: 'Some experimentation I did with Websockets and Anthropics API',
+      imageUrl: 'https://portfolio-info-site-csdcd.s3.eu-west-1.amazonaws.com/project/data-entry/home_edited.png',
+      tag: 'Websockets',
+      buttonLink: '/megpt'
     },
   ];
 
--- src/app/pages/projects/data-entry/data-entry.component.ts
+++ src/app/pages/projects/data-entry/data-entry.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-data-entry',
   standalone: true,
   imports: [],
   templateUrl: './data-entry.component.html',
-  styleUrl: './data-entry.component.css'
+  styleUrls: ['./data-entry.component.css']
 })
 export class DataEntryComponent {
+
+  router = '/data-entry'
+
+ 
 
 }
